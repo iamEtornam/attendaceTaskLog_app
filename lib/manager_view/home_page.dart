7bd0eccd2daff67ac13e8433d2aaec9fbf048372
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-23 21:49:41 +0000

    Create home_page.dart

---
+++ lib/manager_view/home_page.dart
@@ -0,0 +1,96 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         appBar: AppBar(
+        centerTitle: true,
+        title: Text('Fasyl Tech. (Manager)',style: TextStyle(fontSize: 20),),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: (){
+
+              },
+                        child: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.black,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+              child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context,index){
+return InkWell(
+  onTap: (){
+    debugPrint('click $index');
+  },
+  child:   Padding(
+     padding: const EdgeInsets.fromLTRB(8,4,8,4),
+    child: Row(
+      children: <Widget>[
+          Container(
+            width: 10,
+            height: 100, 
+            child: SizedBox(),
+            decoration: BoxDecoration(borderRadius: BorderRadius.horizontal(left: Radius.circular(8)),
+            color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+            ),
+            ),
+            Expanded(
+                        child: Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                color: Colors.black.withOpacity(.05),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                Expanded(
+                                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('Full name $index',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
+                    SizedBox(height: 8,),
+                    Text('Department $index')
+                  ],
+                  ),
+                )
+                  ],
+                ),
+              ),
+            )
+          
+      ],
+    ),
+  ),
+);
+          },
+        ),
+      ),
+    );
+  }
+}
