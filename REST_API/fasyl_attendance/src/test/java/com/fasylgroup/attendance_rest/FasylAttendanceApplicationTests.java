da8167bf3e0bd1c489c62e15bf5555888af70b01
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 15:03:10 +0000

    Create FasylAttendanceApplicationTests.java

---
+++ REST_API/fasyl_attendance/src/test/java/com/fasylgroup/attendance_rest/FasylAttendanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fasylgroup.attendance_rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FasylAttendanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
