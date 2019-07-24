45695e165f6d76d80b50d9b2016b5544745f606d
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 23:45:49 +0000

    Update User.php

--- REST_API/fasyl_attendance_api/app/User.php
+++ REST_API/fasyl_attendance_api/app/User.php
@@ -15,9 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,15 +23,18 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function managers()
+    {
+        return $this->hasOne(Manager::class);
+    }
 }
