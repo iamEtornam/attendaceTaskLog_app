b3828a30169c582bf6bb623188eb23dae2c0bd41
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 23:45:54 +0000

    Update 2014_10_12_000000_create_users_table.php

--- REST_API/fasyl_attendance_api/database/migrations/2014_10_12_000000_create_users_table.php
+++ REST_API/fasyl_attendance_api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->string('department');
+            $table->string('photo');
             $table->timestamps();
+
+            $table->unsignedInteger('manager_id');
+
+            $table->foreign('manager_id')->references('id')->on('managers');
         });
     }
 
