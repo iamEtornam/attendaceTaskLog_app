8ee95de575c534dbac80becfb4156f1b7cc1828d
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 17:58:50 +0000

    Update FasylAttendanceApplication.java

--- REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/FasylAttendanceApplication.java
+++ REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/FasylAttendanceApplication.java
@@ -2,8 +2,10 @@ package com.fasylgroup.attendance_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FasylAttendanceApplication {
 
 	public static void main(String[] args) {
