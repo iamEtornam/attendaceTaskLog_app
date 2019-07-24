976ff95bd1c2ad390396f5c89a8c04f8eaa85d8f
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 17:58:36 +0000

    Update UserRepository.java

--- REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/repository/UserRepository.java
+++ REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.fasylgroup.attendance_rest.repository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fasylgroup.attendance_rest.model.User;
@@ -12,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    @Query(value = "select * from users where id = ?",nativeQuery = true)
+    ArrayList<User> findCurrentUser(Long id);
 }
