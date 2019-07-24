5f83d5cf79a5402725ce9bcaedfe235bd23f2098
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 15:03:55 +0000

    Create FasylAttendanceApplication.java

---
+++ REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/FasylAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.fasylgroup.attendance_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasylAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasylAttendanceApplication.class, args);
+	}
+
+}
