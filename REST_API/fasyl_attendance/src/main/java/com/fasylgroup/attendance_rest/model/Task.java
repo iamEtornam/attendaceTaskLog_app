3c9fe14356a5fd5f3655d5309e442a1fad20b912
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 15:51:27 +0000

    Create Task.java

---
+++ REST_API/fasyl_attendance/src/main/java/com/fasylgroup/attendance_rest/model/Task.java
@@ -0,0 +1,47 @@
+package com.fasylgroup.attendance_rest.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, 
+        allowGetters = true)
+public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+    
+    @NotBlank
+    private Date startTime;
+    
+    @NotBlank
+    private Date endTime;
+    
+    //foreign_key (user id) here
+    
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+}
