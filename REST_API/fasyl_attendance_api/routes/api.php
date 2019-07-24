ad577529b7eb6e6b3bba96dce12371ae882a388c
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-24 21:54:44 +0000

    Create api.php

---
+++ REST_API/fasyl_attendance_api/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('user_login', 'AuthController@login');
+Route::post('user_register', 'AuthController@register');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'AuthController@details');
+
+    Route::resource('products', 'ProductController');
+});
