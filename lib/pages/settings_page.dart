8bf0026f65c4746fc21b408a5a5c375a740c393a
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-25 10:58:15 +0000

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -16,52 +17,60 @@ class _SettingsPageState extends State<SettingsPage> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: ListView(
-        children: <Widget>[
-                   ListTile(
-            onTap: () {
-            },
-            title: Text('FAQ'),
-            leading: Icon(AntDesign.getIconData('questioncircle'))
-          ),
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 8),
-  child:   Divider(),
-),
-          ListTile(
-            onTap: () {
-            },
-            title: Text('Contact Us'),
-            leading: Icon(
-                Platform.isIOS ? Ionicons.getIconData('ios-contacts'):Icons.people
-                  )
-          ),
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 8),
-  child:   Divider(),
-),
-       ListTile(
-            onTap: () {
-            },
-            title: Text('Terms & Privacy Policy'),
-            leading: Icon(Icons.security)
-          ),
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 8),
-  child:   Divider(),
-),
-       ListTile(
-            onTap: () {
-            },
-            title: Text('Licenses'),
-            leading: Icon(Entypo.getIconData('news'))
-          ),
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 8),
-  child:   Divider(),
-)
-        ],
-      ),
+          children: <Widget>[
+            ListTile(
+                onTap: () {
+                  _launchURL('');
+                },
+                title: Text('FAQ'),
+                leading: Icon(AntDesign.getIconData('questioncircle'))),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Divider(),
+            ),
+            ListTile(
+                onTap: () {
+                  _launchURL('');
+                },
+                title: Text('Contact Us'),
+                leading: Icon(Platform.isIOS
+                    ? Ionicons.getIconData('ios-contacts')
+                    : Icons.people)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Divider(),
+            ),
+            ListTile(
+                onTap: () {
+                  _launchURL('');
+                },
+                title: Text('Terms & Privacy Policy'),
+                leading: Icon(Icons.security)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Divider(),
+            ),
+            ListTile(
+                onTap: () {
+                  _launchURL('');
+                },
+                title: Text('Licenses'),
+                leading: Icon(Entypo.getIconData('news'))),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Divider(),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
