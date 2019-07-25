1c05a86448326f4dcefcc83179b9bbb5923267b8
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-25 01:49:18 +0000

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,18 @@
+import 'package:fasyl_attendence_app/core/authentication.dart';
 import 'package:fasyl_attendence_app/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State < LoginPage > {
-  final _formKey = GlobalKey < FormState > ();
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
 
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
@@ -30,168 +34,203 @@ class _LoginPageState extends State < LoginPage > {
         isVisibleIcon = Icons.visibility;
       });
     }
-
   }
 
+  @override
+  void initState() {
+    isUserAuthenticated();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                children: < Widget > [
-                  FlutterLogo(
-                    size: 80,
-                    colors: Colors.blueGrey,
-                  ),
-                  SizedBox(height: 10, ),
-                  Center(child: Text('Fasyl Technologies', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500), )),
-                  SizedBox(height: 100),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: < Widget > [
-                        TextFormField(
-                          focusNode: _emailFocusNode,
-                          controller: _emailController,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16.0,
-                          ),
-                          cursorColor: Colors.black,
-                          keyboardType: TextInputType.emailAddress,
-                          maxLines: 1,
-                          enableInteractiveSelection: true,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Entypo.getIconData('email')),
-                            hasFloatingPlaceholder: true,
-                            hintText: 'Email',
-                            hintStyle: TextStyle(color: Colors.black),
-                            contentPadding: EdgeInsets.all(15.0),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            errorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            focusedErrorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            fillColor: Colors.white,
-                            filled: true,
-                            labelStyle: TextStyle(color: Colors.black),
-                            errorStyle: TextStyle(
-                              color: Colors.red,
-                            ),
-                          ),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your Email';
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 10, ),
-                        TextFormField(
-                          focusNode: _passwordFocusNode,
-                          controller: _passwordController,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16.0,
-                          ),
-                          cursorColor: Colors.black,
-                          obscureText: isObscured,
-                          keyboardType: TextInputType.text,
-                          maxLines: 1,
-                          enableInteractiveSelection: true,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Feather.getIconData('lock')),
-                            suffixIcon: InkWell(
-                              onTap: () {
-                                makeVisible();
-                              },
-                              child: Icon(isVisibleIcon)),
-                            hasFloatingPlaceholder: true,
-                            hintText: 'Password',
-                            hintStyle: TextStyle(color: Colors.black),
-                            contentPadding: EdgeInsets.all(15.0),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            errorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            focusedErrorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red),
-                              borderRadius: BorderRadius.all(Radius.circular(10))),
-                            fillColor: Colors.white,
-                            filled: true,
-                            labelStyle: TextStyle(color: Colors.black),
-                            errorStyle: TextStyle(
-                              color: Colors.red,
-                            ),
-                          ),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter your Email';
-                            }
-                            return null;
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 25, ),
-                  InkWell(
-                    onTap: () async {
-                      if (_formKey.currentState.validate()) {
-                        await processCredentials(_emailController.text, _passwordController.text);
-                      } else {
-                        alertNotification(context, Colors.red, 'Fields cannot be Empty');
-      Navigator.of(context).pushReplacementNamed('/dashboard');
-
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
+          child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            children: <Widget>[
+              FlutterLogo(
+                size: 80,
+                colors: Colors.blueGrey,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: Text(
+                'Fasyl Technologies',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              )),
+              SizedBox(height: 100),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      focusNode: _emailFocusNode,
+                      controller: _emailController,
+                      style: TextStyle(
                         color: Colors.black,
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        fontSize: 16.0,
                       ),
-                      child: Center(child: Text('Login', style: TextStyle(color: Colors.white), )),
+                      cursorColor: Colors.black,
+                      keyboardType: TextInputType.emailAddress,
+                      maxLines: 1,
+                      enableInteractiveSelection: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Entypo.getIconData('email')),
+                        hasFloatingPlaceholder: true,
+                        hintText: 'Email',
+                        hintStyle: TextStyle(color: Colors.black),
+                        contentPadding: EdgeInsets.all(15.0),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelStyle: TextStyle(color: Colors.black),
+                        errorStyle: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your Email';
+                        }
+                        return null;
+                      },
                     ),
-                  )
-                ],
-            ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      focusNode: _passwordFocusNode,
+                      controller: _passwordController,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                      ),
+                      cursorColor: Colors.black,
+                      obscureText: isObscured,
+                      keyboardType: TextInputType.text,
+                      maxLines: 1,
+                      enableInteractiveSelection: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Feather.getIconData('lock')),
+                        suffixIcon: InkWell(
+                            onTap: () {
+                              makeVisible();
+                            },
+                            child: Icon(isVisibleIcon)),
+                        hasFloatingPlaceholder: true,
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.black),
+                        contentPadding: EdgeInsets.all(15.0),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelStyle: TextStyle(color: Colors.black),
+                        errorStyle: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your Email';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              InkWell(
+                onTap: () async {
+                  if (_formKey.currentState.validate()) {
+                    await processCredentials(
+                        _emailController.text, _passwordController.text);
+                  } else {
+                    alertNotification(
+                        context, Colors.red, 'Fields cannot be Empty');
+                  }
+                },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+              )
+            ],
           ),
-        )
-      ),
+        ),
+      )),
     );
   }
 
+  //validate user name and password
   processCredentials(String email, String password) async {
     bool emailValid =
-      RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+        RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
     if (password.isNotEmpty && emailValid) {
-      Navigator.of(context).pushReplacementNamed('/managerView');
+      emailPasswordLogin(context, email, password).then((_) {
+        Navigator.of(context).pushReplacementNamed('/dashboard');
+      });
     } else if (password.isEmpty) {
       alertNotification(context, Colors.red, 'Password cannot be Empty.');
     } else if (!emailValid) {
       alertNotification(context, Colors.red, 'Enter a valid Email Address.');
     } else {
       alertNotification(context, Colors.red, 'Something went wrong.');
+    }
+  }
 
+  //check if user is already authenticated
+  void isUserAuthenticated() async{
+    FirebaseUser user = await _auth.currentUser();
+    if(user != null){
+      Navigator.of(context).pushReplacementNamed('/dashboard');
     }
   }
 }
