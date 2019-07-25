f6bc6e0d0e0c5759e02c625e68c202f51357e432
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-07-25 14:41:49 +0000

    Update reports_page.dart

--- lib/pages/reports_page.dart
+++ lib/pages/reports_page.dart
@@ -1,16 +1,17 @@
-
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:fasyl_attendence_app/pages/fragments/completed_fragment.dart';
+import 'package:fasyl_attendence_app/pages/fragments/delayed_fragment.dart';
+import 'package:fasyl_attendence_app/pages/fragments/in_progress_fragment.dart';
+import 'package:fasyl_attendence_app/pages/fragments/uncompleted_fragment.dart';
 import 'package:flutter/material.dart';
-import 'package:pie_chart/pie_chart.dart';
 
 class ReportsPage extends StatefulWidget {
   @override
   _ReportsPageState createState() => _ReportsPageState();
 }
 
-class _ReportsPageState extends State<ReportsPage> with SingleTickerProviderStateMixin{
-Map<String, double> dataMap = new Map();
+class _ReportsPageState extends State<ReportsPage>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
   final List<Tab> tabs = <Tab>[
     Tab(text: 'Completed'),
@@ -19,14 +20,10 @@ Map<String, double> dataMap = new Map();
     Tab(text: 'Uncompleted'),
   ];
 
-@override
-  void initState() {
-        _tabController = new TabController(vsync: this, length: tabs.length);
 
-    dataMap.putIfAbsent('Uncompleted', () => 3);
-dataMap.putIfAbsent('In-Progress', () => 3);
-dataMap.putIfAbsent('Completed', () => 8);
-dataMap.putIfAbsent('Delayed', () => 2);
+  @override
+  void initState() {
+    _tabController = new TabController(vsync: this, length: tabs.length);
     super.initState();
   }
 
@@ -37,33 +34,30 @@ dataMap.putIfAbsent('Delayed', () => 2);
         padding: const EdgeInsets.all(12.0),
         child: Column(
           children: <Widget>[
-            PieChart(dataMap: dataMap) ,
-            SizedBox(height: 15,),
-            TabBar(
-            isScrollable: true,
-            unselectedLabelColor: Colors.grey,
-            labelColor: Colors.white,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicator: BubbleTabIndicator(
-              indicatorHeight: 30.0,
-              indicatorColor: Colors.black,
-              tabBarIndicatorSize: TabBarIndicatorSize.tab,
-            ),
-            tabs: tabs,
-            controller: _tabController,
+          TabBar(
+          isScrollable: true,
+          unselectedLabelColor: Colors.grey,
+          labelColor: Colors.white,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicator: BubbleTabIndicator(
+            indicatorHeight: 30.0,
+            indicatorColor: Colors.black,
+            tabBarIndicatorSize: TabBarIndicatorSize.tab,
           ),
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: <Widget>[
-                CompletedFragment(),
-                CompletedFragment(),
-                CompletedFragment(),
-                CompletedFragment(),
-
-              ],
-            ),
-          )
+          tabs: tabs,
+          controller: _tabController,
+        ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  CompletedFragment(),
+                  InProgressFragment(),
+                  DelayedFragment(),
+                  UncompletedFragment(),
+                ],
+              ),
+            )
           ],
         ),
       ),
