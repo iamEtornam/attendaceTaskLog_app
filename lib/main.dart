9dcb33ff743baf64811c68a27a1afb492aa84693
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-08-25 08:49:00 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fasyl Attendance',
+      title: 'Attendance-Task',
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColorDark: Colors.black,
